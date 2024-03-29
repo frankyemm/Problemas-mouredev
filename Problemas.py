f2ccc536bc7cf1d7c1766493e548e41286a46786
frankyemm <159089341+frankyemm@users.noreply.github.com>
2024-03-29 12:23:01 -0500

    Update Problemas.py

--- Problemas.py
+++ Problemas.py
@@ -6,6 +6,8 @@
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  *
+
+ """
  for i in range (1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
@@ -15,7 +17,6 @@
         print("buzz")
     else:
         print(i)
- """
 """Problema 2* anagrama
  * Escribe una función que reciba dos palabras (String) y retorne
  * verdadero o falso (Bool) según sean o no anagramas.
@@ -24,6 +25,7 @@
  * - NO hace falta comprobar que ambas palabras existan.
  * - Dos palabras exactamente iguales no son anagrama.
  *
+ """
 def anagrama(string1, string2):
     dict1 = {}
     dict2 = {}
@@ -41,13 +43,13 @@ def anagrama(string1, string2):
         return True
     else:
         return False
- """
 """Problema 3* fibonacci
  * Escribe un programa que imprima los 50 primeros números de la sucesión
  * de Fibonacci empezando en 0.
  * - La serie Fibonacci se compone por una sucesión de números en
  *   la que el siguiente siempre es la suma de los dos anteriores.
- *   0, 1, 1, 2, 3, 5, 8, 13...
+ *   0, 1, 1, 2, 3, 5, 8, 13... 
+"""
 def fibonacci(n):
     counter = 0
     fibopas = 0
@@ -59,24 +61,24 @@ def fibonacci(n):
             print(fibof)
             fibopas = fibopre
             fibopre = fibof
-            fibof = fibopas + fibopre 
-"""
+            fibof = fibopas + fibopre
 """Problema 4* es primo
 *Escribe un programa que se encargue de comprobar si un número es o no primo.
 * Hecho esto, imprime los números primos entre 1 y 100.
 *
+"""
 def primo(n):
     for i in range(2, n):
         if n % i == 0:
             return False
     return True
-"""
 """Problema 5* área del polígono
  * Crea una única función (importante que sólo sea una) que sea capaz
  * de calcular y retornar el área de un polígono.
  * - La función recibirá por parámetro sólo UN polígono a la vez.
  * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
  * - Imprime el cálculo del área de un polígono de cada tipo.
+"""
 def area(p, a):
     try:
         result = float(p)*float(a)/2
@@ -87,14 +89,13 @@ print("Recuerda que el perímetro es la suma de los lados y, que la apotema es l
 p = input("ingrese el perímetro de la figura: ")
 a= input("ingrese la apotema de la figura: ")
 print(area(p,a))
-"""
 """Problema 7* invertir cadena
  * Crea un programa que invierta el orden de una cadena de texto
  * sin usar funciones propias del lenguaje que lo hagan de forma automática.
  * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+"""
 def invertir(string):
     return string[::-1]
-"""
 """Problema 8* contador de palabras
  * Crea un programa que cuente cuantas veces se repite cada palabra
  * y que muestre el recuento final de todas ellas.
@@ -102,6 +103,7 @@ def invertir(string):
  * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
  * - No se pueden utilizar funciones propias del lenguaje que
  *   lo resuelvan automáticamente.
+"""
 def counterword(string):
     alphabet = {"A": "a","B": "b","C": "c","D": "d","E": "e","F": "f","G": "g","H": "h","I": "i","J": "j","K": "k","L": "l","M": "m","N": "n","O": "o","P": "p","Q": "q","R": "r","S": "s","T": "t","U": "u","V": "v","W": "w","X": "x","Y": "y","Z": "z"}
     symbol = [".", ",", ":", ";", "?", "!", "-", "'", "\"", "(", ")", "[", "]", "{", "}", "/", "\\", "<", ">"]
@@ -130,18 +132,17 @@ def counterword(string):
         print(i, words[i])
         total_words += words[i]
     return print(f"Hay un total de {total_words} palabras")
-"""
 """Problema 9* de decimal a binario
  * Crea un programa se encargue de transformar un número
  * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
  *
+"""
 def decBin(number):
     number_string = ""
     while number > 1:
         number_string += str(number % 2)
         number = number//2
     return "1"+number_string[::-1]
-"""
 """Problema 10* conversion morse_natural
  * Crea un programa que sea capaz de transformar texto natural a código
  * morse y viceversa.
@@ -152,6 +153,7 @@ def decBin(number):
  * - El alfabeto morse soportado será el mostrado en
  *   https://es.wikipedia.org/wiki/Código_morse.
  *
+"""
 def conv(text):
     en_alphabet = {"A": ".-","B": "-...","C": "-.-.","D": "-..","E": ".","F": "..-.","G": "--.","H": "....","I": "..","J": ".---","K": "-.-","L": ".-..","M": "--","N": "-.","O": "---","P": ".--.","Q": "--.-","R": ".-.","S": "...","T": "-","U": "..-","V": "...-","W": ".--","X": "-..-","Y": "-.--","Z": "--..","0": "-----","1": ".----","2": "..---","3": "...--","4": "....-","5": ".....","6": "-....","7": "--...","8": "---..","9": "----.",".": ".-.-.-",",": "--..--","?": "..--..","'": ".----.","!": "-.-.--","/": "-..-.","(": "-.--.",")": "-.--.-","&": ".-...",":": "---...",";": "-.-.-.","=": "-...-","+": ".-.-.","-": "-....-","_": "..--.-","\"": ".-..-.","$": "...-..-","@": ".--.-.", " ": "       "}
     conversion = ""
@@ -168,7 +170,6 @@ def conv(text):
         return print(f"Morse to natural is: \n{conversion}")
     except (ValueError, KeyError, IndexError, SyntaxError) as e:
         return print("An error has occurred: ", e) 
-"""
 """Problema 11* comprobar ecuación balanceada
  * Crea un programa que comprueba si los paréntesis, llaves y corchetes
  * de una expresión están equilibrados.
@@ -179,6 +180,7 @@ def conv(text):
  * - Expresión balanceada: { [ a * ( c + d ) ] - 5 }
  * - Expresión no balanceada: { a * ( c + d ) ] - 5 }
  *
+"""
 def balanced(expresion):
     sqr_b = ""
     par = ""
@@ -200,7 +202,6 @@ def balanced(expresion):
         return sqr_b, par, curl_b
     except (KeyError, ValueError, TypeError, SyntaxError) as e:
         return e
-"""
 """Problema 12* en uno sí y en otro no str
  * Crea una función que reciba dos cadenas como parámetro (str1, str2)
  * e imprima otras dos cadenas como salida (out1, out2).
@@ -209,6 +210,7 @@ def balanced(expresion):
  * - out2 contendrá todos los caracteres presentes en la str2 pero NO
  *   estén presentes en str1.
  *
+"""
 def chains(str1, str2):
     out1 = ""
     out2 = ""
@@ -219,7 +221,6 @@ def chains(str1, str2):
         if j not in str1:
             out2 += j
     return out1, out2
-"""
 """Problema 13* palíndromo
  * Escribe una función que reciba un texto y retorne verdadero o
  * falso (Boolean) según sean o no palíndromos.
@@ -228,6 +229,7 @@ def chains(str1, str2):
  * NO se tienen en cuenta los espacios, signos de puntuación y tildes.
  * Ejemplo: Ana lleva al oso la avellana.
  *
+"""
 def palindrome(text):
     symbol = [".", ",", ":", ";", "?", "!", "-", "'", "\"", "(", ")", "[", "]", "{", "}", "/", "\\", "<", ">"]
     text = text.split()
@@ -241,11 +243,11 @@ def palindrome(text):
         return True
     else:
         return False
-"""
 """Problema 14* factorial recursivo
  * Escribe una función que calcule y retorne el factorial de un número dado
  * de forma recursiva.
  *
+"""
 def recursived_factorial(n):
     try:
         n = int(n)
@@ -257,13 +259,13 @@ def recursived_factorial(n):
             return n*recursived_factorial(n-1)
     except (KeyError, TypeError, ValueError, SyntaxError) as e:
         return e
-"""
 """Problema 15* número narcisista
  * Escribe una función que calcule si un número dado es un número de Armstrong
  * (o también llamado narcisista).
  * Si no conoces qué es un número de Armstrong, debes buscar información
  * al respecto.
  *
+"""
 def narcisist(n):
     sum = 0
     for i in str(n):
@@ -273,7 +275,6 @@ def narcisist(n):
         return True
     else:
         return False
-"""
 """Problema 16*
  * Crea una función que calcule y retorne cuántos días hay entre dos cadenas
  * de texto que representen fechas.
